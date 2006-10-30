ad340d6e8511b011c89e3d34daff4d89c50c81b7
barras <barras@0cf17b13-060f-0410-b1b1-c666bec9822a>
2006-10-30 12:26:09 +0000

    usage of legacy field
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@525 0cf17b13-060f-0410-b1b1-c666bec9822a

--- Tactiques.v
+++ Tactiques.v
@@ -1,5 +1,6 @@
 Require Import definition.
 Require Export LegacyZArithRing.
+Require Export LegacyRfield.
 
 Ltac RingReplace x y := 
   change x with y || (replace x with y; [ idtac | legacy ring ]).
